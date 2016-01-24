7f92f275f8663d2b6cfc4d047b72e3444ac8f7f1
smashoth <ploofn@gmail.com>
2016-01-23 20:40:48 -0500

    Update to bitwise operations

--- CSelfStudy/src/Ch2-9.c
+++ CSelfStudy/src/Ch2-9.c
@@ -4,10 +4,33 @@
  *  Created on: Jan 23, 2016
  *      Author: smashoth
  */
-#include <stdio.h>
 
-int main (){
-	puts("This is dope!!");
-	return 0;
+
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned getbits(unsigned x, int p, int n);
+
+int main(void) {
+	unsigned x = 198;
+	int p = 4;
+	int n = 3;
+	puts("Breakdown:");
+	printf("p + 1 -n = %d\n", p + 1 - n);
+	printf("x >> 2 = %08x\n", x >> 2);
+	printf("~0 << n = %x\n", ~0 << n);
+	printf("~(ff ff ff f8) = %08x\n", ~(0xfffffff8));
+	printf("0x31 & 0x07 = %08x\n", 0x31 & 0x07);
+
+	x = getbits(x, p, n);
+	printf("bitfield contains: %08x\n", x);
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
 }
+
+unsigned getbits(unsigned x, int p, int n){
+
+	return (x >> (p +1 - n)) & ~(~0 << n);
+}
+
 
