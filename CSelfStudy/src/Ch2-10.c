82cfde8ad1ef531e9f6806d93319f4fc417ccb66
smashoth <ploofn@gmail.com>
2016-01-23 20:28:30 -0500

    Chapter 2.9 and 2.10

---
+++ CSelfStudy/src/Ch2-10.c
@@ -0,0 +1,14 @@
+/*
+ * Ch2-10.c
+ *
+ *  Created on: Jan 23, 2016
+ *      Author: smashoth
+ */
+
+
+#include <stdio.h>
+
+int main(){
+	puts("This is bomb");
+	return 0;
+}
---
+++ CSelfStudy/src/Ch2-9.c
@@ -0,0 +1,13 @@
+/*
+ * Ch2-9.c
+ *
+ *  Created on: Jan 23, 2016
+ *      Author: smashoth
+ */
+#include <stdio.h>
+
+int main (){
+	puts("This is dope!!");
+	return 0;
+}
+
